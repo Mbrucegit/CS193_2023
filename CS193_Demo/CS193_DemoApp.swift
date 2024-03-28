64fbe5613ba5f5d9115e11ec6960f822eb5a5f8e
Qiang Fu <tactiturn@hotmail.com>
2024-03-29 02:27:53 +0800

    Initial Commit

---
+++ CS193_Demo/CS193_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193_DemoApp.swift
+//  CS193_Demo
+//
+//  Created by Michael on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CS193_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS193_Demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CS193_Demo
+//
+//  Created by Michael on 3/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
